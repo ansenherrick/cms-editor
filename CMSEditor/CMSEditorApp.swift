f48050f65742a8f0947278570f2ec47230770f51
ansenherrick <ansenherrick@gmail.com>
2026-08-17 18:17:02 -0400

    Initial Commit

---
+++ CMSEditor/CMSEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CMSEditorApp.swift
+//  CMSEditor
+//
+//  Created by Ansen Herrick on 8/17/26.
+//
+
+import SwiftUI
+
+@main
+struct CMSEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CMSEditor/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CMSEditor
+//
+//  Created by Ansen Herrick on 8/17/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
